d0608e2c7752ee63fd8d350821aa99e6dc3aae4d
PejeDev <hi@pejedev.xyz>
2025-05-27 13:00:26 -0500

    ✨feat(turbo): setup monorepo with shadcn and nextjs

---
+++ apps/covehub/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['@cove/ui'],
+  allowedDevOrigins: ['127.0.0.1'],
+}
+
+export default nextConfig
---
+++ apps/covehub/postcss.config.mjs
@@ -0,0 +1,1 @@
+export { default } from '@cove/ui/postcss.config'
